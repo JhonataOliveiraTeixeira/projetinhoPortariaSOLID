57ec891faf88d98e8fbd558845a38f54e1b197a9
JhonataOliveiraTeixeira <jonasdeot@gmail.com>
2024-05-21 17:02:24 -0300

    Projeto portaria SOLID 0.1

---
+++ README.md
@@ -0,0 +1,14 @@
+REQUISITOS FUNCIONAIS
+
+[] Permitir cadastro de usuários 
+[] Permitir cadastro com e-mail e número, sendo e-mail unico 
+[] Permitir deletar usuários
+[] Permitir atualizar usuários
+
+REGRA DE NEGOCIOS
+
+[] Autenticação de usuários
+[] Apartamentos que possuem moradores 
+[] O edificio terá 10 andares e cada andar deve possuir 6 Apartamentos
+[]Relação entre usuário e Apartamentos
+[] usuáriospodem ser moradores ou visitantes
---
+++ src/repositories/prisma/prisma-users-repositories.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/prisma";
+import { Prisma, User } from '@prisma/client'
+import { UserRepository } from "../users-repository";
+
+export class PrimsaUserRepositpries implements UserRepository {
+    async findByEmail(email: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            }
+        })
+        return user
+    }
+
+    async create(data: Prisma.UserCreateInput) {
+        const user = await prisma.user.create({
+            data
+
+        })
+
+        return user
+    }
+}
